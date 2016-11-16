3a75cb52d4982121e164ec0c484c9b33a9f323e8
James Brenton <freshlybakedcode@jamesbrenton.co.uk>
2016-11-16 01:30:32 +0000

    Added images and a bunch of styling

--- js/main.js
+++ js/main.js
@@ -1,1 +1,13 @@
-
+// var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+//
+// window.onload = function() {
+//     document.getElementById("scroll").onclick = function scrollDown() {
+//         console.log(h);
+//         var x = 1;
+//         var y = 1; //delay in milliseconds
+//         setInterval(function() {
+//             window.scroll(0, x);
+//               x = x + 1; //if you want to increase speed simply increase increment interval
+//         }, y);
+//     }
+// }
